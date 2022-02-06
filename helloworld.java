6e6dbaf8251f6fd13cca6ba28ab6e95b8d6d497c
Mohajit Paul <80643937+mohajit2711@users.noreply.github.com>
2022-02-06 18:28:46 +0530

    Create helloworld.java
    Hello World intro with Java and documentation comments

---
+++ helloworld.java
@@ -0,0 +1,8 @@
+public class MyClass {
+    public static void main(String args[]) {
+
+      System.out.println("Hello World"); /*Comment for Hello World
+      as you can see this is also a comment*/
+      /*** Start of my documentation line comment ***/
+    }
+}
