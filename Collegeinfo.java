792bd8a3802629a28d475ed3affa08bf2a73ded5
Mohajit Paul <80643937+mohajit2711@users.noreply.github.com>
2022-02-06 18:34:39 +0530

    Create Collegeinfo.java
    Gives Info about College student

---
+++ Collegeinfo.java
@@ -0,0 +1,15 @@
+public class collegeinfo {
+
+   public static void main(String[] args) {
+       //correct variable declarations
+       String name = "John";
+       int age = 21;
+       boolean isGraduated = false;
+       double averageScore = 4.3;
+       //do not change this code
+       System.out.println(name);
+       System.out.println(age);
+       System.out.println(isGraduated);
+       System.out.println(averageScore);
+   }
+}
