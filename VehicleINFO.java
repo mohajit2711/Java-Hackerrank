0d185cac721e4674430ca7a892e954a24b44c91f
Mohajit Paul <80643937+mohajit2711@users.noreply.github.com>
2022-02-06 18:32:06 +0530

    Create VehicleINFO.java
    Gives info about Toyota car

---
+++ VehicleINFO.java
@@ -0,0 +1,14 @@
+public class Program
+{
+    public static void main(String[] args) {
+        //fix the variable types 
+        String name = "Toyota";
+        double engine = 4.7;
+        int year = 2019;
+       
+        System.out.println("Name: " + name);
+        System.out.println("Engine: " + engine);
+        System.out.println("Year: " + year);
+
+	}
+}
