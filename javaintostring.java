a33f3341ba8e61059d1a694d210d46b86fdf9657
Mohajit Paul <80643937+mohajit2711@users.noreply.github.com>
2022-03-27 12:24:03 +0530

    Create javaintostring.java

---
+++ javaintostring.java
@@ -0,0 +1,18 @@
+ import java.util.*;
+//Mohajit Paul 
+import java.security.*;
+public class Solution {
+ public static void main(String[] args) {
+ 
+  DoNotTerminate.forbidExit();
+ 
+  try {
+   Scanner in = new Scanner(System.in);
+   int n = in .nextInt();
+   in.close();
+   //String s=???; Complete this line below
+ 
+   //Write your code here
+String s = String.valueOf(n);
+// Mohajit Paul 
+ 
