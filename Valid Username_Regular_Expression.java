7422da7022b8f9bdb03b569378f5c4377c2d21e9
Mohajit Paul <80643937+mohajit2711@users.noreply.github.com>
2022-03-31 22:14:10 +0530

    Create Valid Username_Regular_Expression.java

---
+++ Valid Username_Regular_Expression.java
@@ -0,0 +1,10 @@
+//Mohajit Paul 20BCE10630
+import java.util.Scanner;
+//Mohajit Paul 20BCE10630
+class UsernameValidator {
+    /*
+     * Write regular expression here.
+     */
+     public static final String regularExpression =
+ "^[a-zA-Z][a-zA-Z0-9_]{7,29}$";
+}
