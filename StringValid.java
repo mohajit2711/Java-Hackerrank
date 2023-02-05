df97b5c87f4d615a8006ca10dbccc9265b5f76e1
Mohajit Paul <80643937+mohajit2711@users.noreply.github.com>
2023-02-05 09:37:36 -0800

    String Valid or Not using Regex

---
+++ StringValid.java
@@ -0,0 +1,23 @@
+// You are using Java
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Main {
+    public static void main (String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String input = sc.nextLine();
+        
+        String pattern = "[a-zA-Z0-9]+";
+        System.out.println(input);
+        
+        Pattern p = Pattern.compile(pattern);
+        Matcher m = p.matcher(input);
+        
+        if(m.matches()) {
+            System.out.println("true");
+        } else {
+            System.out.println("false");
+        }
+    }
+}
